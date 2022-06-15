ab7d75736c7cb7fe74f29ccec244882185bed775
Françoar Santiago <fransantiago0@gmail.com>
2022-06-15 17:35:56 -0400

    feat: adds Landing page

---
+++ src/pages/landing/index.tsx
@@ -0,0 +1,12 @@
+import Presentation from './presentation';
+import { Wrapper } from './styles';
+
+const Landing: React.FC = () => {
+  return (
+    <Wrapper>
+      <Presentation />
+    </Wrapper>
+  );
+};
+
+export default Landing;
