dd7cd8f6e3133a01eed4688d5b9eae3167a1e88b
Françoar Santiago <fransantiago0@gmail.com>
2022-06-23 21:42:57 -0400

    feat: adds who-am-i page component

---
+++ src/pages/landing/who-am-i/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import DetailsCard from '@/components/details-card';
+import HeaderContent from '@/components/header-content';
+
+import {
+  Cards,
+  ContentBackground,
+  Description,
+  Details,
+  LeftContent,
+  RightContent,
+  Title,
+  Wrapper,
+  YearsOfExperience,
+} from './styles';
+
+const WhoAmI: React.FC = () => {
+  const yearsOfXp = new Date().getFullYear() - 2018;
+
+  return (
+    <Wrapper name="about-me">
+      <HeaderContent section="About me" title="Who Am I?" />
+      <RightContent>
+        <Details>
+          <Title>Françoar Santiago&apos;s details</Title>
+          <Description>
+            <p>
+              I have a Bsc in Computer Science from the UNINORTE (Amazonas,
+              Brazil) and about {yearsOfXp} years of experience with systems
+              development.
+            </p>
+            <p>
+              I&apos;ve been working as Software Engineer and Tech Lead,
+              improving and developing products that integrate functionalities
+              between company departments and track raw materials
+              transformation, like MES systems.
+            </p>
+            <p>Keep scrolling to see more about my tech stack!</p>
+          </Description>
+          <Cards>
+            <DetailsCard title="Name" description="Françoar Santiago" />
+            <DetailsCard title="Email" description="fransantiago0@gmail.com" />
+            <DetailsCard title="LinkedIn" description="/francoarsantiago" />
+            <DetailsCard title="Github" description="/fransantiago" />
+          </Cards>
+        </Details>
+      </RightContent>
+
+      <LeftContent>
+        <ContentBackground>Experience</ContentBackground>
+
+        <YearsOfExperience>
+          <span>{yearsOfXp}+</span>
+          years of experience
+        </YearsOfExperience>
+      </LeftContent>
+    </Wrapper>
+  );
+};
+
+export default WhoAmI;
---
+++ src/pages/landing/who-am-i/styles.ts
@@ -0,0 +1,192 @@
+import { Element } from 'react-scroll';
+import styled from 'styled-components';
+
+import GlobalWrapper from '@/styles/landing-sections-wrapper.styles';
+
+import { ContentWrapper } from '@/components/header-content/styles';
+import breakpoints from '@/constants/breakpoints';
+
+export const Cards = styled.div`
+  display: grid;
+
+  grid-template-columns: repeat(auto-fill, 250px);
+  row-gap: 1rem;
+
+  justify-content: space-between;
+  align-items: start;
+
+  margin-top: 1.4rem;
+
+  @media only screen and (max-width: ${breakpoints.large.width.max}px) {
+    grid-template-columns: 1fr;
+  }
+
+  @media only screen and (max-width: ${breakpoints.small.width.max}px) {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+export const ContentBackground = styled.p`
+  position: absolute;
+  top: 10rem;
+
+  font-family: 'Indie Flower', sans-serif;
+  font-size: 7rem;
+  letter-spacing: 0.2rem;
+
+  color: rgba(255, 255, 255, 0.1);
+
+  transform: rotate(-12deg);
+
+  @media only screen and (max-width: ${breakpoints.xsmall.width.max}px) {
+    font-size: 3.2rem;
+  }
+
+  @media only screen and (max-width: ${breakpoints.small.width.max}px) {
+    position: relative;
+    top: 0;
+
+    margin-top: 2rem;
+  }
+`;
+
+export const Description = styled.div`
+  p {
+    color: var(--secondary-text);
+    font-size: 1.2rem;
+
+    text-align: justify;
+
+    @media only screen and (max-width: ${breakpoints.small.width.max}px) {
+      font-size: 1.2rem;
+      margin-top: 1rem;
+    }
+  }
+`;
+
+export const Details = styled.div`
+  position: absolute;
+  top: 9rem;
+
+  width: 35rem;
+  min-width: 15rem;
+
+  @media only screen and (max-width: ${breakpoints.large.width.max}px) {
+    top: 4rem;
+  }
+
+  @media only screen and (max-width: ${breakpoints.small.width.max}px) {
+    position: relative;
+    top: 0;
+
+    width: 87%;
+  }
+`;
+
+export const LeftContent = styled(ContentWrapper)`
+  position: relative;
+  grid-area: left-content;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  @media only screen and (max-width: ${breakpoints.large.width.max}px) {
+    height: 65vh;
+  }
+
+  @media only screen and (max-width: ${breakpoints.small.width.max}px) {
+    height: 100%;
+  }
+`;
+
+export const RightContent = styled(ContentWrapper)`
+  position: relative;
+  grid-area: right-content;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  padding: 0;
+
+  @media only screen and (max-width: ${breakpoints.large.width.max}px) {
+    height: 70vh;
+  }
+
+  @media only screen and (max-width: ${breakpoints.small.width.max}px) {
+    height: 100%;
+  }
+`;
+
+export const Title = styled.p`
+  font-size: 2rem;
+  font-weight: bold;
+
+  margin-bottom: 1rem;
+
+  @media only screen and (max-width: ${breakpoints.small.width.max}px) {
+    display: none;
+  }
+`;
+
+export const Wrapper = styled(Element)`
+  ${GlobalWrapper};
+
+  background-color: var(--secondary-bg);
+
+  display: grid;
+
+  grid-template-columns: repeat(2, 1fr);
+
+  grid-template-areas:
+    'header-content header-content'
+    'left-content right-content';
+
+  @media only screen and (max-width: ${breakpoints.large.width.max}px) {
+    grid-template-columns: 1fr;
+
+    grid-template-areas:
+      'header-content'
+      'right-content'
+      'left-content';
+  }
+
+  @media only screen and (max-width: ${breakpoints.small.width.max}px) {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+export const YearsOfExperience = styled.p`
+  position: absolute;
+  top: 14rem;
+
+  max-width: 250px;
+  font-size: 3rem;
+
+  span {
+    display: block;
+
+    color: var(--primary-text);
+    font-size: 5rem;
+    text-align: center;
+    font-weight: 600;
+  }
+
+  @media only screen and (max-width: ${breakpoints.small.width.max}px) {
+    position: relative;
+    top: 0;
+
+    font-size: 2.3rem;
+    text-align: center;
+    margin-bottom: 3rem;
+
+    span {
+      font-size: 4rem;
+    }
+  }
+`;
