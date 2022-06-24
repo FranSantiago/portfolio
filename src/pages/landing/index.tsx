fdb64de202f4a099b05b597a517c00ad062a1b00
Françoar Santiago <fransantiago0@gmail.com>
2022-06-23 21:43:35 -0400

    feat: adds who-am-i component to landing page

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,10 +1,14 @@
+import React from 'react';
+
 import Presentation from './presentation';
 import { Wrapper } from './styles';
+import WhoAmI from './who-am-i';
 
 const Landing: React.FC = () => {
   return (
     <Wrapper>
       <Presentation />
+      <WhoAmI />
     </Wrapper>
   );
 };
