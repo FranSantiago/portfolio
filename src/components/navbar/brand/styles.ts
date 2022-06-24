e0690ad0944b6bedab8168494854e7a4bda893ef
Françoar Santiago <fransantiago0@gmail.com>
2022-06-23 21:46:27 -0400

    feat: adds HTML open close tags effect to navbar brand

--- src/components/navbar/brand/styles.ts
+++ src/components/navbar/brand/styles.ts
@@ -2,10 +2,24 @@ import styled from 'styled-components';
 
 export const FirstNameBrand = styled.span`
   font-weight: bold;
+
+  ::before {
+    content: '<';
+
+    font-family: Arial, sans-serif;
+    margin-right: 0.2rem;
+  }
 `;
 
 export const LastNameBrand = styled.span`
   margin-left: 0.4rem;
+
+  ::after {
+    content: '/>';
+
+    font-family: Arial, sans-serif;
+    margin-left: 0.2rem;
+  }
 `;
 
 export const Wrapper = styled.div`
